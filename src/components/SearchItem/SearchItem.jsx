112de9ca28078b9dd9ebc54f8b091afea9794cd0
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-12-08 12:01:23 -0600

    disabled "add favorite" button onClick

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -3,10 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import  Button  from "react-bootstrap/Button";
 
 function SearchItem ({gif}) {
+    const [isClicked, setIsClicked] = useState(false);
     const dispatch = useDispatch();
 
     const dispatchFavorite = () => {
         console.log('url should be:', gif.images.original.url)
+        setIsClicked(true)
         dispatch({
         type: 'SAGA/ADD_NEW_FAVORITE',
         payload: gif.images.original.url
@@ -15,7 +17,7 @@ function SearchItem ({gif}) {
     return (
         <>
         <img src={gif.images.original.url}></img>
-        <Button variant="primary" onClick={dispatchFavorite}>FAV that</Button>
+        <Button variant="primary" disabled={isClicked} onClick={dispatchFavorite}>FAV that</Button>
         </>
     )
 }
